a21bbe9ac6eba7a47f2fdbe1bd95d12f2982d127
Jessica Tsang <jessicatsang@Jessica-Tsangs-MacBook-Pro.local>
2015-09-27 18:49:18 +1000

    Attempt at implementing display() and init()

--- src/ass2/spec/Game.java
+++ src/ass2/spec/Game.java
@@ -23,6 +23,7 @@ import com.jogamp.opengl.util.FPSAnimator;
  */
 public class Game extends JFrame implements GLEventListener{
 
+	// create an array of already loaded textures and just bind the one you need whenever you need it.
     private Terrain myTerrain;
 
     public Game(Terrain terrain) {
@@ -36,16 +37,20 @@ public class Game extends JFrame implements GLEventListener{
      *
      */
     public void run() {
+    	  //init
     	  GLProfile glp = GLProfile.getDefault();
           GLCapabilities caps = new GLCapabilities(glp);
           GLJPanel panel = new GLJPanel();
+          
+          // event listener to handle rendering events
           panel.addGLEventListener(this);
  
           // Add an animator to call 'display' at 60fps        
           FPSAnimator animator = new FPSAnimator(60);
           animator.add(panel);
           animator.start();
-
+          
+          //set panel options
           getContentPane().add(panel);
           setSize(800, 600);        
           setVisible(true);
@@ -68,6 +73,30 @@ public class Game extends JFrame implements GLEventListener{
 	public void display(GLAutoDrawable drawable) {
 		// TODO Auto-generated method stub
 		GL2 gl = drawable.getGL().getGL2();
+    	
+		/************* JESS'S * POTATO * CODE***************/
+		
+		//clear the colour buffer incase of garbage
+		// set background colour
+		gl.glClear(GL2.GL_COLOR_BUFFER_BIT | GL2.GL_DEPTH_BUFFER_BIT);
+		
+		//make the model-view matrix
+		gl.glMatrixMode(GL2.GL_MODELVIEW);
+    	gl.glLoadIdentity();
+    	
+    	// Fill/LineColour
+    	gl.glPolygonMode(GL2.GL_FRONT_AND_BACK, GL2.GL_LINE);
+		gl.glPolygonMode(GL2.GL_FRONT_AND_BACK, GL2.GL_FILL);
+		
+		//turn on back face culling
+		gl.glEnable(GL2.GL_CULL_FACE);
+		gl.glCullFace(GL2.GL_BACK);
+    	
+		//<perspective camera code here>
+		
+		/****************************************************/
+		
+		//draw terrain
 		myTerrain.draw(gl);
 	}
 
@@ -81,6 +110,21 @@ public class Game extends JFrame implements GLEventListener{
 	public void init(GLAutoDrawable drawable) {
 		// TODO Auto-generated method stub
 		
+		/************* JESS'S * POTATO * CODE***************/
+		
+		GL2 gl = drawable.getGL().getGL2();
+
+    	gl.glClearColor(1.0f, 1.0f, 1.0f, 0.0f);
+    	// Enable depth testing
+    	gl.glEnable(GL2.GL_DEPTH_TEST);
+
+    	//Enable lighting
+    	gl.glEnable(GL2.GL_LIGHTING);
+    	// Ambient light source
+    	gl.glEnable(GL2.GL_LIGHT0); 
+    	
+    	/****************************************************/
+    	
 	}
 
 	@Override
