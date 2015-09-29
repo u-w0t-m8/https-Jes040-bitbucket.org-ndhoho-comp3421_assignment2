e71fee8a5823cd849b05e7125076445ce4602ceb
andypho <andyho280392@gmail.com>
2015-09-29 21:57:38 +1000

    Tree done.. kinda?

--- src/ass2/spec/Terrain.java
+++ src/ass2/spec/Terrain.java
@@ -24,238 +24,242 @@ import org.json.JSONTokener;
  */
 public class Terrain {
 
-    private Dimension mySize;
-    private double[][] myAltitude;
-    private List<Tree> myTrees;
-    private List<Road> myRoads;
-    private float[] mySunlight;
+	private Dimension mySize;
+	private double[][] myAltitude;
+	private List<Tree> myTrees;
+	private List<Road> myRoads;
+	private float[] mySunlight;
 
-    /**
-     * Create a new terrain
-     *
-     * @param width The number of vertices in the x-direction
-     * @param depth The number of vertices in the z-direction
-     */
-    public Terrain(int width, int depth) {
-        mySize = new Dimension(width, depth);
-        myAltitude = new double[width][depth];
-        myTrees = new ArrayList<Tree>();
-        myRoads = new ArrayList<Road>();
-        mySunlight = new float[3];
-    }
-    
-    public Terrain(Dimension size) {
-        this(size.width, size.height);
-    }
+	/**
+	 * Create a new terrain
+	 *
+	 * @param width The number of vertices in the x-direction
+	 * @param depth The number of vertices in the z-direction
+	 */
+	public Terrain(int width, int depth) {
+		mySize = new Dimension(width, depth);
+		myAltitude = new double[width][depth];
+		myTrees = new ArrayList<Tree>();
+		myRoads = new ArrayList<Road>();
+		mySunlight = new float[3];
+	}
 
-    public Dimension size() {
-        return mySize;
-    }
+	public Terrain(Dimension size) {
+		this(size.width, size.height);
+	}
 
-    public List<Tree> trees() {
-        return myTrees;
-    }
+	public Dimension size() {
+		return mySize;
+	}
 
-    public List<Road> roads() {
-        return myRoads;
-    }
+	public List<Tree> trees() {
+		return myTrees;
+	}
 
-    public float[] getSunlight() {
-        return mySunlight;
-    }
+	public List<Road> roads() {
+		return myRoads;
+	}
 
-    /**
-     * Set the sunlight direction. 
-     * 
-     * Note: the sun should be treated as a directional light, without a position
-     * 
-     * @param dx
-     * @param dy
-     * @param dz
-     */
-    public void setSunlightDir(float dx, float dy, float dz) {
-        mySunlight[0] = dx;
-        mySunlight[1] = dy;
-        mySunlight[2] = dz;        
-    }
-    
-    /**
-     * Resize the terrain, copying any old altitudes. 
-     * 
-     * @param width
-     * @param height
-     */
-    public void setSize(int width, int height) {
-        mySize = new Dimension(width, height);
-        double[][] oldAlt = myAltitude;
-        myAltitude = new double[width][height];
-        
-        for (int i = 0; i < width && i < oldAlt.length; i++) {
-            for (int j = 0; j < height && j < oldAlt[i].length; j++) {
-                myAltitude[i][j] = oldAlt[i][j];
-            }
-        }
-    }
+	public float[] getSunlight() {
+		return mySunlight;
+	}
 
-    /**
-     * Get the altitude at a grid point
-     * 
-     * @param x
-     * @param z
-     * @return
-     */
-    public double getGridAltitude(int x, int z) {
-        return myAltitude[x][z];
-    }
+	/**
+	 * Set the sunlight direction. 
+	 * 
+	 * Note: the sun should be treated as a directional light, without a position
+	 * 
+	 * @param dx
+	 * @param dy
+	 * @param dz
+	 */
+	public void setSunlightDir(float dx, float dy, float dz) {
+		mySunlight[0] = dx;
+		mySunlight[1] = dy;
+		mySunlight[2] = dz;        
+	}
 
-    /**
-     * Set the altitude at a grid point
-     * 
-     * @param x
-     * @param z
-     * @return
-     */
-    public void setGridAltitude(int x, int z, double h) {
-        myAltitude[x][z] = h;
-    }
+	/**
+	 * Resize the terrain, copying any old altitudes. 
+	 * 
+	 * @param width
+	 * @param height
+	 */
+	public void setSize(int width, int height) {
+		mySize = new Dimension(width, height);
+		double[][] oldAlt = myAltitude;
+		myAltitude = new double[width][height];
 
-    /**
-     * Get the altitude at an arbitrary point. 
-     * Non-integer points should be interpolated from neighbouring grid points
-     * 
-     * TO BE COMPLETED
-     *  Using equation from:
-     *  http://www.ajdesigner.com/phpinterpolation/bilinear_interpolation_equation.php
-     * 
-     * @param x
-     * @param z
-     * @return
-     */
-    public double altitude(double x, double z) {
-        double altitude = 0;
-        
-        int x1 = (int) Math.floor(x);
-        int x2 = (int) Math.ceil(x);
-        int z1 = (int) Math.floor(z);
-        int z2 = (int) Math.ceil(z);
-        
-        double a11 = getGridAltitude(x1,z1);
-        double a21 = getGridAltitude(x2,z1);
-        double a12 = getGridAltitude(x1,z2);
-        double a22 = getGridAltitude(x2,z2);
+		for (int i = 0; i < width && i < oldAlt.length; i++) {
+			for (int j = 0; j < height && j < oldAlt[i].length; j++) {
+				myAltitude[i][j] = oldAlt[i][j];
+			}
+		}
+	}
 
-        double p1 = a11 * (((x2 - x) * (z2 - z)) / ((x2 - x1) * (z2 - z1)));
-        double p2 = a21 * (((x - x1) * (z2 - z)) / ((x2 - x1) * (z2 - z1)));
-        double p3 = a12 * (((x2 - x) * (z - z1)) / ((x2 - x1) * (z2 - z1)));
-        double p4 = a22 * (((x - x1) * (z - z1)) / ((x2 - x1) * (z2 - z1)));
+	/**
+	 * Get the altitude at a grid point
+	 * 
+	 * @param x
+	 * @param z
+	 * @return
+	 */
+	public double getGridAltitude(int x, int z) {
+		return myAltitude[x][z];
+	}
 
-        altitude = p1 + p2 + p3 + p4;
-        
-        return altitude;
-    }
-    
-    /**
-     * Add a tree at the specified (x,z) point. 
-     * The tree's y coordinate is calculated from the altitude of the terrain at that point.
-     * 
-     * @param x
-     * @param z
-     */
-    public void addTree(double x, double z) {
-        double y = altitude(x, z);
-        Tree tree = new Tree(x, y, z);
-        myTrees.add(tree);
-    }
+	/**
+	 * Set the altitude at a grid point
+	 * 
+	 * @param x
+	 * @param z
+	 * @return
+	 */
+	public void setGridAltitude(int x, int z, double h) {
+		myAltitude[x][z] = h;
+	}
 
+	/**
+	 * Get the altitude at an arbitrary point. 
+	 * Non-integer points should be interpolated from neighbouring grid points
+	 * 
+	 * TO BE COMPLETED
+	 *  Using equation from:
+	 *  http://www.ajdesigner.com/phpinterpolation/bilinear_interpolation_equation.php
+	 * 
+	 * @param x
+	 * @param z
+	 * @return
+	 */
+	public double altitude(double x, double z) {
+		double altitude = 0;
 
-    /**
-     * Add a road. 
-     * 
-     * @param x
-     * @param z
-     */
-    public void addRoad(double width, double[] spine) {
-        Road road = new Road(width, spine);
-        myRoads.add(road);        
-    }
+		int x1 = (int) Math.floor(x);
+		int x2 = (int) Math.ceil(x);
+		int z1 = (int) Math.floor(z);
+		int z2 = (int) Math.ceil(z);
+
+		double a11 = getGridAltitude(x1,z1);
+		double a21 = getGridAltitude(x2,z1);
+		double a12 = getGridAltitude(x1,z2);
+		double a22 = getGridAltitude(x2,z2);
+
+		double p1 = a11 * (((x2 - x) * (z2 - z)) / ((x2 - x1) * (z2 - z1)));
+		double p2 = a21 * (((x - x1) * (z2 - z)) / ((x2 - x1) * (z2 - z1)));
+		double p3 = a12 * (((x2 - x) * (z - z1)) / ((x2 - x1) * (z2 - z1)));
+		double p4 = a22 * (((x - x1) * (z - z1)) / ((x2 - x1) * (z2 - z1)));
+
+		altitude = p1 + p2 + p3 + p4;
+
+		return altitude;
+	}
+
+	/**
+	 * Add a tree at the specified (x,z) point. 
+	 * The tree's y coordinate is calculated from the altitude of the terrain at that point.
+	 * 
+	 * @param x
+	 * @param z
+	 */
+	public void addTree(double x, double z) {
+		double y = altitude(x, z);
+		Tree tree = new Tree(x, y, z);
+		myTrees.add(tree);
+	}
 
 
-    /**
-     * Drawing a mesh of triangles
-     * 
-     * Example:
-     * (0,0,0)  (1,0.5,0)   
-     * 		+-----+
-     * 		|    /|  
-     * 		|  /  |
-     * 		|/    |
-     * 		+-----+
-     * 	(0,0,1)  (1,0.3,1)
-     * (x, y, z) where y = altitudes
-     * counter-clockwise for front facing
-     * @param gl
-     */
-    public void draw(GL2 gl){
-    	
-    	for(int x = 0; x < mySize.getWidth()-1; x++){
-    		for(int z = 0; z < mySize.getHeight()-1; z++){
-    			// x0 x1 x2
-    			// y0 y1 y2
-    			// z0 z1 z2
-    			double[][] LeftTriangle = {{x, x, x+1},
-    								{getGridAltitude(x,z), getGridAltitude(x,z+1), getGridAltitude(x+1,z)},
-    								{z, z+1, z}};
-    			double[][] RightTriangle = {{x+1, x+1, x},
-    								{getGridAltitude(x+1,z+1), getGridAltitude(x+1,z), getGridAltitude(x,z+1)},
-    								{z+1, z, z+1}};
-    			
-    			double [] LeftTriNormal = calcNormal(LeftTriangle, 2);
-    			double [] RightTriNormal = calcNormal(RightTriangle, 2);
-    			
-    			gl.glBegin(GL2.GL_TRIANGLES);
-    				gl.glNormal3d(LeftTriNormal[0], LeftTriNormal[1], LeftTriNormal[2]);
-    				gl.glVertex3d(LeftTriangle[0][0], LeftTriangle[1][0], LeftTriangle[2][0]);
-    				gl.glVertex3d(LeftTriangle[0][1], LeftTriangle[1][1], LeftTriangle[2][1]);
-    				gl.glVertex3d(LeftTriangle[0][2], LeftTriangle[1][2], LeftTriangle[2][2]);
-    			gl.glEnd();
-    			
-    			gl.glBegin(GL2.GL_TRIANGLES);
+	/**
+	 * Add a road. 
+	 * 
+	 * @param x
+	 * @param z
+	 */
+	public void addRoad(double width, double[] spine) {
+		Road road = new Road(width, spine);
+		myRoads.add(road);        
+	}
+
+
+	/**
+	 * Drawing a mesh of triangles
+	 * 
+	 * Example:
+	 * (0,0,0)  (1,0.5,0)   
+	 * 		+-----+
+	 * 		|    /|  
+	 * 		|  /  |
+	 * 		|/    |
+	 * 		+-----+
+	 * 	(0,0,1)  (1,0.3,1)
+	 * (x, y, z) where y = altitudes
+	 * counter-clockwise for front facing
+	 * @param gl
+	 */
+	public void draw(GL2 gl){
+
+		for(int x = 0; x < mySize.getWidth()-1; x++){
+			for(int z = 0; z < mySize.getHeight()-1; z++){
+				// x0 x1 x2
+				// y0 y1 y2
+				// z0 z1 z2
+				double[][] LeftTriangle = {{x, x, x+1},
+						{getGridAltitude(x,z), getGridAltitude(x,z+1), getGridAltitude(x+1,z)},
+						{z, z+1, z}};
+				double[][] RightTriangle = {{x+1, x+1, x},
+						{getGridAltitude(x+1,z+1), getGridAltitude(x+1,z), getGridAltitude(x,z+1)},
+						{z+1, z, z+1}};
+
+				double [] LeftTriNormal = calcNormal(LeftTriangle, 2);
+				double [] RightTriNormal = calcNormal(RightTriangle, 2);
+
+				gl.glBegin(GL2.GL_TRIANGLES);
+					gl.glNormal3d(LeftTriNormal[0], LeftTriNormal[1], LeftTriNormal[2]);
+					gl.glVertex3d(LeftTriangle[0][0], LeftTriangle[1][0], LeftTriangle[2][0]);
+					gl.glVertex3d(LeftTriangle[0][1], LeftTriangle[1][1], LeftTriangle[2][1]);
+					gl.glVertex3d(LeftTriangle[0][2], LeftTriangle[1][2], LeftTriangle[2][2]);
+				gl.glEnd();
+
+				gl.glBegin(GL2.GL_TRIANGLES);
 					gl.glNormal3d(RightTriNormal[0], RightTriNormal[1], RightTriNormal[2]);
 					gl.glVertex3d(RightTriangle[0][0], RightTriangle[1][0], RightTriangle[2][0]);
-    				gl.glVertex3d(RightTriangle[0][1], RightTriangle[1][1], RightTriangle[2][1]);
-    				gl.glVertex3d(RightTriangle[0][2], RightTriangle[1][2], RightTriangle[2][2]);
-    			gl.glEnd();
-    			
-    			}
-    		}
-    	}
+					gl.glVertex3d(RightTriangle[0][1], RightTriangle[1][1], RightTriangle[2][1]);
+					gl.glVertex3d(RightTriangle[0][2], RightTriangle[1][2], RightTriangle[2][2]);
+				gl.glEnd();
 
-    /**
-     * Newell's Method for computing face normal. lecture note 04_3D page 25
-     * 
-     * @param triangleCoord
-     * @param vertices
-     * @return
-     */
+			}
+		}
+		
+		for(Tree tree: myTrees){
+			tree.draw(gl);
+		}
+	}
+
+	/**
+	 * Newell's Method for computing face normal. lecture note 04_3D page 25
+	 * 
+	 * @param triangleCoord
+	 * @param vertices
+	 * @return
+	 */
 	public double[] calcNormal(double[][] triangleCoord, int vertices) {
 		double nx = 0;
 		double ny = 0;
 		double nz = 0;
-		
+
 		for(int i = 0; i < vertices; i++){
 			nx += (triangleCoord[1][i] - triangleCoord[1][i+1]) * (triangleCoord[2][i] + triangleCoord[2][i+1]);
 		}
-		
+
 		for(int j = 0; j < vertices; j++){
 			ny += (triangleCoord[2][j] - triangleCoord[2][j+1]) * (triangleCoord[0][j] + triangleCoord[0][j+1]);
 		}
-		
+
 		for(int k = 0; k < vertices; k++){
 			nz += (triangleCoord[0][k] - triangleCoord[0][k+1]) * (triangleCoord[1][k] - triangleCoord[1][k+1]);
 		}
-		
+
 		double[] normal = {nx, ny, nz};
 		return normal;
 	}
-    
+
 }
--- src/ass2/spec/Tree.java
+++ src/ass2/spec/Tree.java
@@ -1,5 +1,9 @@
 package ass2.spec;
 
+import javax.media.opengl.GL2;
+import javax.media.opengl.glu.GLU;
+import javax.media.opengl.glu.GLUquadric;
+
 /**
  * COMMENT: Comment Tree 
  *
@@ -9,6 +13,13 @@ public class Tree {
 
     private double[] myPos;
     
+	double height = 1;
+	double cylinderRadius = 0.1;
+	double sphereRadius = 0.5;
+	
+	private static final int SLICES = 32;
+	private static final int STACKS = 50;
+	
     public Tree(double x, double y, double z) {
         myPos = new double[3];
         myPos[0] = x;
@@ -20,5 +31,31 @@ public class Tree {
         return myPos;
     }
     
+    /**
+     * 
+     * https://www.opengl.org/sdk/docs/man2/xhtml/gluCylinder.xml
+     * @param gl
+     */
+    public void draw(GL2 gl){
+    	GLU glu = new GLU();
+    	System.out.println("DrawTree");
+    	gl.glPushMatrix();
+    	
+        gl.glTranslated(myPos[0], myPos[1], myPos[2]);        
+        gl.glRotated(270, 1.0, 0.0, 0.0);
+        
+        GLUquadric cylinder = glu.gluNewQuadric();
+        glu.gluQuadricNormals(cylinder, GL2.GL_SMOOTH);
+        glu.gluCylinder(cylinder, cylinderRadius, cylinderRadius, height, SLICES, STACKS);
+        
 
+		gl.glRotated(90, 1.0, 0.0, 0.0);
+		gl.glTranslated(0.0, height, 0.0);
+
+        GLUquadric sphere = glu.gluNewQuadric();
+        glu.gluQuadricNormals(sphere, GL2.GL_SMOOTH);
+        glu.gluSphere(sphere, sphereRadius, SLICES, STACKS);
+
+        gl.glPopMatrix();
+    }
 }
